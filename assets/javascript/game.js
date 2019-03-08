5a3bff2029892d326b6687121bf7fc1691867709
kbub27 <kalebmerriman27@gmail.com>
2019-03-07 21:44:27 -0800

    working game no style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,10 +3,13 @@ var computerChoices = ["q","w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s"
 var wins = 0;
 var losses = 0;
 var remaining = 10;
+var choices = [];
 
 var userChoice = document.getElementById("choices");
-var userLoss = document.getElementById("youlose");
+var userLoss = document.getElementById("youlost");
 var userWin = document.getElementById("youwin");
+var userRemain = document.getElementById("remaining");
+
 
 document.onkeyup = function(event) {
 
@@ -14,13 +17,22 @@ document.onkeyup = function(event) {
 
     let computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-    if (userGuess === computerGuess) {
-        wins++;
-        console.log(wins);
-    } else {
+    choices.push(userGuess)
+
+    if ( userGuess !== computerGuess && remaining > 1) {
+        remaining--;
+    } else if ( userGuess !== computerGuess && remaining < 2 ) {
         losses++;
-        console.log(losses)
+        remaining = + 10;
+        choices = [];
+    } else {
+        wins++;
+        choices = [];
     }
 
-    
+
+    userWin.textContent = wins;
+    userLoss.textContent = losses;
+    userRemain.textContent = remaining;
+    userChoice.textContent = choices;
 }
